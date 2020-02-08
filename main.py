56af3ce8e942d8a5ab3b3e15095129f216b62e01
Cache McClure <cmcclure@agstrata.com>
2020-02-08 15:01:56 -0600

    created a top menu script to access all the fx

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 from cache_fx import build_sis_config
 from cache_fx import build_sql_config
 from cache_fx import sis_auth_chk
+from cache_fx import ret_token
+from cache_fx import token_chk
+from cache_fx import query_elast
+from cache_fx import geo_code
+from cache_fx import get_dist
+from cache_fx import sql_query
+import pandas as pd
+import sys
 
 def zero():
     print('help statement here')
-    return
+    return main()
 
 def one():
     uname = input('Sisense username: ')
@@ -16,7 +24,10 @@ def one():
         build_sis_config(uname,pword)
     else:
         build_sis_config(uname,pword,endpoint=ep)
-    return
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
 
 def two():
     uname = input('SQL username: ')
@@ -33,29 +44,109 @@ def two():
         build_sql_config(uname,pword,database=dbn)
     else:
         build_sql_config(uname,pword,address=adre,database=dbn)
-    return
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
 
 def three():
     config = sis_auth_chk()
-    token = ret_token(config[0],config[1],endPoint=config[2])
+    token = ret_token(config['username'],config['password'],endPoint=config['endpoint'])
     print(token)
-    return
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
+
+def four():
+    config = token_chk()
+    token = config['token']
+    config2 = sis_auth_chk()
+    endpoint = config2['endpoint']
+    datasource = input('Elasticube name: ')
+    query = input('Query: ')
+    save_nm = input('Please enter a name for the output file: ')
+    output = query_elast(datasource,query,token,endPoint=endpoint)
+    output.to_csv(save_nm+'.csv')
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
+
+def five():
+    config = token_chk()
+    token = config['token']
+    config2 = sis_auth_chk()
+    endpoint = config2['endpoint']
+    city = input('City: ')
+    state = input('State: ')
+    out = geo_code(token,city,state)
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
+
+def six():
+    config = token_chk()
+    token = config['token']
+    config2 = sis_auth_chk()
+    endpoint = config2['endpoint']
+    city1 = input('First city: ')
+    state1 = input('First state: ')
+    city2 = input('Second city: ')
+    state2 = input('Second state: ')
+    out = get_dist(token,city1,state1,city2,state2,endPoint=endpoint)
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
+
+def seven():
+    config = sql_auth_chk()
+    username = config['username']
+    password = config['password']
+    address = config['address']
+    database = config['database']
+    table = input('Table name: ')
+    query = input('Query: ')
+    save_nm = input('Please enter a name for the output file: ')
+    out = sql_query(query,username,password,table,address=address,database=database)
+    out.to_csv(save_nm+'.csv')
+    print(' ')
+    input('Press enter to return to the main menu')
+    print(' ')
+    return main()
 
 def switch_fx(argument):
-    switcher = {}
+    switcher = {
+        1: one,
+        2: two,
+        3: three,
+        4: four,
+        5: five,
+        6: six,
+        7: seven,
+        9: sys.exit
+        }
+    func = switcher.get(argument, lambda: "Invalid entry")
+    return func()
+
+def main():
+    print("WELCOME TO THE TOP MENU")
+    print(' ')
+    print('MENU')
+    print('0 - Help')
+    print('1 - Build Sisense Configuration file')
+    print('2 - Build SQL Configuration file')
+    print('3 - Retrieve Sisense token')
+    print('4 - Query Sisense Elasticube')
+    print('5 - Geo-code a location')
+    print('6 - Find distance between two locations')
+    print('7 - Query SQL database')
+    print('9 - Exit')
+
+    var = input('Choose Option: ')
+    switch_fx(int(var))
     return
 
-print("WELCOME TO THE TOP MENU")
-print(' ')
-print('MENU')
-print('0 - Help')
-print('1 - Build Sisense Configuration file')
-print('2 - Build SQL Configuration file')
-print('3 - Retrieve Sisense token')
-print('4 - Query Sisense Elasticube')
-print('5 - Geo-code a location')
-print('6 - Find distance between two locations')
-print('7 - Query SQL database')
-print('9 - Exit')
-
-var = input('Choose Option')
+main()
